eebb30aec136076d1bfcd28e46a579530b92b983
xiewulong <xiewulong@vip.qq.com>
2015-05-10 11:04:59 +0800

    express

--- controllers/Kd100Controller.php
+++ controllers/Kd100Controller.php
@@ -30,7 +30,8 @@ class Kd100Controller extends Controller{
 		if($express && isset($_POST['param'])){
 			$param = Json::decode($_POST['param']);
 			if(isset($param['lastResult']) && $express->company == $param['lastResult']['com'] && $express->number == $param['lastResult']['nu'] && (empty($express->auth_key) || (isset($_POST['sign']) && $this->module->manager->verify($_POST['sign'], $_POST['param'], $express->auth_key)))){
-				$express->data = Json::encode($param['lastResult']['data']);
+				$express->status = $manager->getStatus($param['lastResult']['state']);
+				$express->details = Json::encode($param['lastResult']['data']);
 				if($param['lastResult']['ischeck'] == 1 && $param['lastResult']['state'] == 3){
 					$express->receipted_at = strtotime($param['lastResult']['data'][0]['time']);
 				}
